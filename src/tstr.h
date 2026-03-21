7262b5364443f7daf50cfc9a0a7559f8caab52dc
Totto16 <tobiausgais@gmail.com>
2026-03-21 17:04:47 +0100

    add static literal macro

--- src/tstr.h
+++ src/tstr.h
@@ -282,11 +282,15 @@ TSTR_FUN_ATTRIBUTES [[nodiscard]] tstr tstr_from(const char* cstr);
 #define TSTR_SIZE_OF_STR_LIT(str) ((sizeof(str) - 1) + (REQUIRE_STRING_LITERAL(str)))
 
 // Macro for compile-time string literals (avoids runtime strlen).
+#define TSTR_STATIC_LIT(str) ((tstr_static){ .ptr = (str), .len = TSTR_SIZE_OF_STR_LIT(str) })
+
 #define TSTR_LIT(str) \
 	((tstr){ .type = { .inner = tstr_type_enum_static }, \
 	         .static_str = (tstr_static){ .ptr = (str), .len = TSTR_SIZE_OF_STR_LIT(str) } })
 
 #if defined(__GNUC__) && (!(defined(__clang__)))
+	#define TSTR_STATIC_LIT_CONST(str) { .ptr = (str), .len = TSTR_SIZE_OF_STR_LIT(str) }
+
 	#define TSTR_LIT_CONST(str) \
 		{ \
 			.type = { .inner = tstr_type_enum_static }, .static_str = { \
@@ -295,6 +299,7 @@ TSTR_FUN_ATTRIBUTES [[nodiscard]] tstr tstr_from(const char* cstr);
 			} \
 		}
 #else
+	#define TSTR_STATIC_LIT_CONST(str) TSTR_STATIC_LIT(str)
 	#define TSTR_LIT_CONST(str) TSTR_LIT(str)
 #endif
 
